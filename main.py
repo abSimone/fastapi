7ced41aed02694ff26f06daf3da1704946ecb41b
abSimone <s.angelonibei@digitality-consulting.com>
2022-11-02 15:44:52 +0100

    aggiunto commento

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from controller import actors, films
 
-from fastapi.middleware.cors import CORSMiddleware
+#from fastapi.middleware.cors import CORSMiddleware
 
 # In questa variabile ci sta l'istanza che tiene in piedi l'applicazione
 app = FastAPI()
